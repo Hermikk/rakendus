36d62a161390e223d369a9626d0e731a199bf653
Hermikk <60460305+Hermikk@users.noreply.github.com>
2020-06-10 20:53:00 +0300

    Add files via upload

---
+++ tund5/summary_log.php
@@ -0,0 +1,37 @@
+<?php
+require ("../../../../configuration.php");
+require ("fnc_news.php");
+
+require("classes/Session.class.php");
+SessionManager::sessionStart("vr20", 0, "/~mikk.herde/", "tigu.hk.tlu.ee");
+
+//kas on sisse loginud
+if(!isset($_SESSION["userid"])) {
+    //jõuga avalehele
+    header("Location: page.php");
+}
+
+//login välja
+if(isset($_GET["logout"])){
+    session_destroy();
+    header("Location: page.php");
+}
+
+
+$summaryHTML = readLogsAll();
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="utf-8">
+    <title>Õppelogi</title>
+</head>
+<body>
+    <h1>Õppelogi</h1>
+<?php echo $summaryHTML; ?>
+<hr>
+<p>Logi <a href="?logout=1">välja!</a></p>
+<p>Tagasi <a href="home.php">avalehele</a>!</p>
+
+</body>
+</html>
